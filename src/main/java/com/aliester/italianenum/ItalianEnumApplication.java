b11dd093c53884d7647708a2f632f38a554deb7c
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-10-12 19:36:32 +0200

    firts commit

---
+++ src/main/java/com/aliester/italianenum/ItalianEnumApplication.java
@@ -0,0 +1,13 @@
+package com.aliester.italianenum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItalianEnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItalianEnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliester/italianenum/ItalianEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliester.italianenum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalianEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
