c67f88e9c6324ca2f496b267fd39478c31f4c066
alejandro99billi <96229780+alejandro99billi@users.noreply.github.com>
2024-02-29 21:33:10 +0100

    improve swagger

--- src/main/java/com/aliester/italianenum/web/controller/MunicipalityController.java
+++ src/main/java/com/aliester/italianenum/web/controller/MunicipalityController.java
@@ -3,6 +3,12 @@ package com.aliester.italianenum.web.controller;
 import com.aliester.italianenum.exception.MunicipalityNotFoundException;
 import com.aliester.italianenum.model.Municipality;
 import com.aliester.italianenum.service.impl.LocationServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +17,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/municipalities")
 @RequiredArgsConstructor
+@Tag(name = "Municipality", description = "Single municipality")
 public class MunicipalityController {
 
     private final LocationServiceImpl service;
 
+
+    @Operation(
+            summary = "Retrieve a municipality",
+            description = "Get a municipality from italy",
+            tags = { "Municipality", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Municipality.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/{municipalityCode}")
     public Municipality getMunicipality(@PathVariable String municipalityCode){
         return service.getMunicipality(municipalityCode);
--- src/main/java/com/aliester/italianenum/web/controller/ProvinceController.java
+++ src/main/java/com/aliester/italianenum/web/controller/ProvinceController.java
@@ -4,6 +4,13 @@ import com.aliester.italianenum.exception.MunicipalitiesNotFoundException;
 import com.aliester.italianenum.model.Municipality;
 import com.aliester.italianenum.model.Province;
 import com.aliester.italianenum.service.impl.LocationServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +21,32 @@ import java.util.List;
 @RestController
 @RequestMapping("api/provinces")
 @RequiredArgsConstructor
+@Tag(name = "Provinces and Municipalities", description = "Return all provinces and all municipalities")
 public class ProvinceController {
 
     private final LocationServiceImpl service;
     @GetMapping()
+    @Operation(
+            summary = "Retrieve all provinces",
+            description = "Get a the provinces from italy",
+            tags = { "Provinces and Municipalities", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content( array = @ArraySchema(schema = @Schema(implementation = Province.class)), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     public List<Province> getProvinces(){
         return service.getProvinces();
     }
 
     @GetMapping("/{provinceCode}/municipalities")
+    @Operation(
+            summary = "Retrieve all municipalities from a provinces",
+            description = "Get a the municipalities from a province",
+            tags = { "Provinces and Municipalities", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(array = @ArraySchema(schema = @Schema(implementation = Municipality.class)), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     public List<Municipality> getMunicipalities(@PathVariable String provinceCode){
         return service.getMunicipalities(provinceCode);
     }
